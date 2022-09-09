5c86b15757d1188376ac0b88af9d0866b432b405
danilo <d.pirola@patente.it>
2022-09-09 16:08:27 +0200

    del unused variables

--- src/Navigator.js
+++ src/Navigator.js
@@ -5,27 +5,25 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const LoggedUser = () => {  
+const LoggedUser = () => {
     const {
-    isLoading,
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
+        isAuthenticated,
+        user,
+        loginWithRedirect,
+        logout,
+    } = useAuth0();
 
-	if (isAuthenticated)
-		return (
-    		<div>Hello {user.name}{' '}
-        	<Button onClick={() => logout({ returnTo: window.location.origin })}>Log out</Button>
-    		</div>);	
-	else
-		return (<Button onClick={loginWithRedirect}>Log in</Button>); 
+    if (isAuthenticated)
+        return (
+            <div>Hello {user.name}{' '}
+                <Button onClick={() => logout({ returnTo: window.location.origin })}>Log out</Button>
+            </div>);
+    else
+        return (<Button onClick={loginWithRedirect}>Log in</Button>);
 }
 
 function Navigator(props) {
-    
+
     return (
         <div className="Nav">
             <Navbar bg="dark" variant="dark" fixed="top">
@@ -35,9 +33,9 @@ function Navigator(props) {
                     <Nav.Link href="/scrapy">Scrapy</Nav.Link>*/}
                 </Nav>
                 <Form inline method="GET" action="/Search">
-                    <FormControl name="id" type="text" placeholder="Titolo film" className="mr-sm-2" defaultValue={props.selection}/>
+                    <FormControl name="id" type="text" placeholder="Titolo film" className="mr-sm-2" defaultValue={props.selection} />
                     <Button variant="outline-info" type="submit">Search</Button>
-                    <LoggedUser/>
+                    <LoggedUser />
                 </Form>
             </Navbar>
         </div>
